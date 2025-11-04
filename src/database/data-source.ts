414dac77e8b06a7daeafa59fafe84a5c0cc968ce
felcast <felipe.castillo@epayco.com>
2025-11-04 13:10:24 -0400

    TASK-001 Feat: Creating migrations

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -8,8 +8,8 @@ const dataSource = new DataSource({
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_NAME || 'medsy',
-  entities: [Status],
-  migrations: ['dist/migrations/*.js'],
+  entities: [Status, 'src/**/*.entity.ts', 'dist/**/*.entity.js'],
+  migrations: ['src/database/migrations/*.ts', 'dist/database/migrations/*.js'],
   synchronize: false,
 });
 
